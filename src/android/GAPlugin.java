411c3776dbddf032f85e25a4e6170f82f1a1674e
mbahl <mbahl@outlook.com>
2015-03-20 11:28:31 -0700

    logs

--- src/android/GAPlugin.java
+++ src/android/GAPlugin.java
@@ -16,39 +16,48 @@ public class GAPlugin extends CordovaPlugin {
 
         if (action.equals("initGA")) {
             try {
+                alert('initGA');
+
                 tracker = ga.getTracker(args.getString(0));
                 /*GAServiceManager.getInstance().setDispatchPeriod(args.getInt(1));*/
                 GAServiceManager.getInstance().setLocalDispatchPeriod(args.getInt(1));
                 callback.success("initGA - id = " + args.getString(0) + "; interval = " + args.getInt(1) + " seconds");
                 return true;
             } catch (final Exception e) {
+                alert('exception e = ' + e);
                 callback.error(e.getMessage());
             }
         } else if (action.equals("exitGA")) {
             try {
+                alert('exitGA');
                 /*GAServiceManager.getInstance().dispatch(); */
                 GAServiceManager.getInstance().dispatchLocalHits();
                 callback.success("exitGA");
                 return true;
             } catch (final Exception e) {
+                alert('exception e = ' + e);
                 callback.error(e.getMessage());
             }
         } else if (action.equals("trackEvent")) {
             try {
+                alert('trackEvent');
                 /*tracker.sendEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3));  */
                 tracker.send(MapBuilder.createEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3)).build());
                 callback.success("trackEvent - category = " + args.getString(0) + "; action = " + args.getString(1) + "; label = " + args.getString(2) + "; value = " + args.getInt(3));
                 return true;
             } catch (final Exception e) {
+                alert('exception e = ' + e);
                 callback.error(e.getMessage());
             }
         } else if (action.equals("trackPage")) {
             try {
+                alert('trackPage');
                 /*tracker.sendView(args.getString(0));*/
                 tracker.send(MapBuilder.createAppView().set(Fields.SCREEN_NAME,args.getString(0)).build());
                 callback.success("trackPage - url = " + args.getString(0));
                 return true;
             } catch (final Exception e) {
+                alert('exception e = ' + e);
                 callback.error(e.getMessage());
             }
         } else if (action.equals("setVariable")) {
@@ -59,6 +68,7 @@ public class GAPlugin extends CordovaPlugin {
                 callback.success("setVariable passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
                 return true;
             } catch (final Exception e) {
+                alert('exception e = ' + e);
                 callback.error(e.getMessage());
             }
         }
@@ -69,6 +79,7 @@ public class GAPlugin extends CordovaPlugin {
                 callback.success("setDimension passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
                 return true;
             } catch (final Exception e) {
+                alert('exception e = ' + e);
                 callback.error(e.getMessage());
             }
         }
@@ -79,6 +90,7 @@ public class GAPlugin extends CordovaPlugin {
                 callback.success("setVariable passed - index = " + args.getInt(2) + "; key = " + args.getString(0) + "; value = " + args.getString(1));
                 return true;
             } catch (final Exception e) {
+                alert('exception e = ' + e);
                 callback.error(e.getMessage());
             }
         }
