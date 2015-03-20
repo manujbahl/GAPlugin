52995eed07d4159c836547f3232bad2d7b54f4d3
mbahl <mbahl@outlook.com>
2015-03-20 12:02:07 -0700

    s

--- src/android/GAPlugin.java
+++ src/android/GAPlugin.java
@@ -7,8 +7,12 @@ import org.apache.cordova.CordovaPlugin;          // modificato da org.apache.co
 import org.apache.cordova.CallbackContext;          // modificato org.apache.cordova.CallbackContext a org.apache.cordova.api.CallbackContext
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class GAPlugin extends CordovaPlugin {
+    public Log log = LogFactory.getLog(GAPlugin.class);
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException{
         GoogleAnalytics ga = GoogleAnalytics.getInstance(cordova.getActivity());
@@ -16,7 +20,7 @@ public class GAPlugin extends CordovaPlugin {
 
         if (action.equals("initGA")) {
             try {
-                alert("initGA");
+                log.debug("initGA");
 
                 tracker = ga.getTracker(args.getString(0));
                 /*GAServiceManager.getInstance().setDispatchPeriod(args.getInt(1));*/
@@ -24,40 +28,40 @@ public class GAPlugin extends CordovaPlugin {
                 callback.success("initGA - id = " + args.getString(0) + "; interval = " + args.getInt(1) + " seconds");
                 return true;
             } catch (final Exception e) {
-                alert("exception e = " + e);
+                //alert("exception e = " + e);
                 callback.error(e.getMessage());
             }
         } else if (action.equals("exitGA")) {
             try {
-                alert("exitGA");
+                //alert("exitGA");
                 /*GAServiceManager.getInstance().dispatch(); */
                 GAServiceManager.getInstance().dispatchLocalHits();
                 callback.success("exitGA");
                 return true;
             } catch (final Exception e) {
-                alert("exception e = " + e);
+                //alert("exception e = " + e);
                 callback.error(e.getMessage());
             }
         } else if (action.equals("trackEvent")) {
             try {
-                alert("trackEvent");
+                //alert("trackEvent");
                 /*tracker.sendEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3));  */
                 tracker.send(MapBuilder.createEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3)).build());
                 callback.success("trackEvent - category = " + args.getString(0) + "; action = " + args.getString(1) + "; label = " + args.getString(2) + "; value = " + args.getInt(3));
                 return true;
             } catch (final Exception e) {
-                alert("exception e = " + e);
+                //alert("exception e = " + e);
                 callback.error(e.getMessage());
             }
         } else if (action.equals("trackPage")) {
             try {
-                alert("trackPage");
+                //alert("trackPage");
                 /*tracker.sendView(args.getString(0));*/
                 tracker.send(MapBuilder.createAppView().set(Fields.SCREEN_NAME,args.getString(0)).build());
                 callback.success("trackPage - url = " + args.getString(0));
                 return true;
             } catch (final Exception e) {
-                alert("exception e = " + e);
+                //alert("exception e = " + e);
                 callback.error(e.getMessage());
             }
         } else if (action.equals("setVariable")) {
@@ -68,7 +72,7 @@ public class GAPlugin extends CordovaPlugin {
                 callback.success("setVariable passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
                 return true;
             } catch (final Exception e) {
-                alert("exception e = " + e);
+                //alert("exception e = " + e);
                 callback.error(e.getMessage());
             }
         }
@@ -79,7 +83,7 @@ public class GAPlugin extends CordovaPlugin {
                 callback.success("setDimension passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
                 return true;
             } catch (final Exception e) {
-                alert("exception e = " + e);
+                //alert("exception e = " + e);
                 callback.error(e.getMessage());
             }
         }
@@ -90,7 +94,7 @@ public class GAPlugin extends CordovaPlugin {
                 callback.success("setVariable passed - index = " + args.getInt(2) + "; key = " + args.getString(0) + "; value = " + args.getString(1));
                 return true;
             } catch (final Exception e) {
-                alert("exception e = " + e);
+                //alert("exception e = " + e);
                 callback.error(e.getMessage());
             }
         }
